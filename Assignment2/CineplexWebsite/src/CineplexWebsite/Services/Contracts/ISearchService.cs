3f574d1bbd8332136a86f1420edc2e3f64b9ea14
Inksprout <candy.goodison@redbubble.com>
2017-05-14 19:37:28 +1000

    implementing sessions page

---
+++ Assignment2/CineplexWebsite/src/CineplexWebsite/Services/Contracts/ISearchService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CineplexWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineplexWebsite.Services.Contracts
+{
+    public interface ISearchService
+    {
+        Task<IActionResult> GetSessionsByMovieTitle(string movieTitle);
+        Task<IActionResult> GetSessionsByCinema(string cinema);
+    }
+}
---
+++ Assignment2/CineplexWebsite/src/CineplexWebsite/Tests/Services/SearchServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CineplexWebsite.Services;
+using CineplexWebsite.Services.Contracts;
+using Xunit;
+
+namespace CineplexWebsite.Tests.Services
+{
+    public class SearchServiceTests
+    {
+        private ISearchService _sut;
+
+        public SearchServiceTests()
+        {
+           //_sut = new SearchService();
+        }
+
+        [Fact]
+        public void GetSessionsByMovieTitle_ADefaultSession_ReturnsTwilightSession()
+        {
+            //Arrange
+
+            //Act
+            var result = _sut.GetSessionsByMovieTitle("fuck you m8");
+
+            //Assert
+          //  Assert.Equal(result, "twilight is on at 9 o clock");
+        }
+    }
+}
